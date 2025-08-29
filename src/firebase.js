b4f4ba53393d0a3e075a07e1a7f4d82801fa34b4
Yasas Priyamantha <yasas.2018489@iit.ac.lk>
2025-08-29 19:31:42 +0530

    Restore .gitignore and update project files

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,21 @@ import { getFirestore } from "firebase/firestore";
 
 // ✅ Your Firebase config (use exactly what Firebase gave you)
 const firebaseConfig = {
-  apiKey: "AIzaSyDgZedPvEDhsUncRu7EitGkT3ISov4sTsU",
-  authDomain: "prime-market-mail.firebaseapp.com",
-  projectId: "prime-market-mail",
-  storageBucket: "prime-market-mail.firebasestorage.app",
-  messagingSenderId: "953809119384",
-  appId: "1:953809119384:web:aff6bda92dea5a26fec8ed",
-  measurementId: "G-EXG2MBDL7Z",
+  //apiKey: "AIzaSyDgZedPvEDhsUncRu7EitGkT3ISov4sTsU",
+  //authDomain: "prime-market-mail.firebaseapp.com",
+  //projectId: "prime-market-mail",
+  //storageBucket: "prime-market-mail.firebasestorage.app",
+  //messagingSenderId: "953809119384",
+  //appId: "1:953809119384:web:aff6bda92dea5a26fec8ed",
+  //measurementId: "G-EXG2MBDL7Z",
+
+  apiKey: "AIzaSyD1Q6CrRV18dwYUENKJghawPw0-b3_up1I",
+  authDomain: "prime-market-mail-303d5.firebaseapp.com",
+  projectId: "prime-market-mail-303d5",
+  storageBucket: "prime-market-mail-303d5.firebasestorage.app",
+  messagingSenderId: "181922298248",
+  appId: "1:181922298248:web:4aa2bb866ff7140978623d",
+  measurementId: "G-Y6FLC6VPJ4"
 };
 
 // Initialize Firebase
